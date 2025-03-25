907d3c4001382aa82a5bc791ce846a4fac449d42
nextgen-2025 <digitalnextgeninfratech@gmail.com>
2025-03-25 15:16:37 +0530

    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Hero from "./components/Hero/Hero";
 import Services from "./components/Services/Serives";
-import Footer from "./components/Footer/Footer";
 import Navbar from "./components/navbar/Navbar";
 
 const App = () => {
@@ -10,7 +9,6 @@ const App = () => {
       <Navbar />
       <Hero />
       <Services />
-      <Footer />
     </>
   );
 };

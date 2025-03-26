ab2830637a81bcce3030c0e260af809975bc2dda
nextgen-2025 <digitalnextgeninfratech@gmail.com>
2025-03-26 19:35:40 +0530

    remove bottom form app

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,16 @@ import { Routes,Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
-import Bottom from "./components/Bottom/Bottom";
 
 
 const App = () => {
   return (
     <>
-
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path='/contact' element={<Contact />} />
       <Route path="*" element={<NotFound /> } />
     </Routes>
-    <Bottom/>
-
     </>
   );
 };

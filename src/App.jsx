06383bd034bfa21c57544e08a109463de92463ef
nextgen-2025 <digitalnextgeninfratech@gmail.com>
2025-03-29 17:46:47 +0530

    error of navbar fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
-import Navbar from "./components/Navbar/Navbar";
 import Bottom from "./components/Bottom/Bottom";
 
 const App = () => {
